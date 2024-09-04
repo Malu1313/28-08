dc7fceca1476a44b19032eff12cc511cb9e79de7
Malu1313 <159253667+Malu1313@users.noreply.github.com>
2024-09-04 12:47:34 -0300

    Add files via upload

---
+++ ControllerNumber.js
@@ -0,0 +1,12 @@
+module.exports = class NumberController{
+    static async checkPar(req,res){
+        res.status(400).json({message:"numero inválido"});
+
+    }
+    if(numbers % 2 === 0){
+        res.status(400).json({message:"Este numero é par"});
+    } 
+    else{
+        res.status
+    }
+}
